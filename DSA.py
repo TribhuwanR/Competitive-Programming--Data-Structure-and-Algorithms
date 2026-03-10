02c26e24bafd1650aba13d945662ed60db781f7f
TribhuwanR <111368052+TribhuwanR@users.noreply.github.com>
2026-03-10 13:24:20 +0530

    Update DSA.py

--- DSA.py
+++ DSA.py
@@ -1,1 +1,28 @@
 
+# Topic1. What are all topics in DS:
+------------------------------------
+# Answer: 1. time complexities
+#         2. space complexities
+#         3. Arrays
+#         4. LinkedList
+#         5. Trees
+#         6. Stacks
+#         7. Queue
+#         8. Dynamic Programming
+#         9. Graphs
+
+
+# Topic2. Topics that are asked in Interview for Data Engineering:
+------------------------------------------------------------------
+#         1. Time Complexity
+#         2. Space Complexity
+#         3.  Arrays
+#         4.  LinkedList
+#         5.  an overview of Trees
+#         6.  Dynamic Programming( 3 to 4 problems we will solve)
+
+
+# Time Complexity:
+------------------
+#                 How much solutions takes time to get completed.
+
